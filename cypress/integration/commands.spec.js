9efb8dcb4835d0305e5c3e230204a519be3e60b1
Angga <angga.trianto@students.amikom.ac.id>
2022-06-06 01:19:13 +0700

    command

---
+++ cypress/integration/commands.spec.js
@@ -0,0 +1,18 @@
+describe('Commands Example', ()=>{
+    beforeEach(()=>{
+        cy.visit('https://www.saucedemo.com/')
+    })
+
+    it('Successfully login test', () => {
+        cy.typeLogin('standard_user', 'secret_sauce')
+        cy.get('.title').should('contain.text', 'Products');
+        cy.logout();
+        cy.url().should('eq', 'https://www.saucedemo.com/')
+    });
+
+    it('Faild Login test', () => {
+        cy.typeLogin('problem_user', 'dummypassword')
+        cy.get('h3').should('contain.text', 'Epic sadface: Username and password do not match any user in this service')
+
+    });
+});
